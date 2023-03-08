aa998a46f6cb18f09f7ca5b4447f9903bff14a4e
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-03-08 22:15:55 +0300

    add game API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+import postData from './modules/dataAPI.js';
+import getData from './modules/getData.js';
+import render from './modules/render.js';
+
+const form = document.querySelector('.score-form');
+const refresh = document.querySelector('#refresh_btn');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postData();
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
+});
+refresh.addEventListener('click', (e) => {
+  e.preventDefault();
+  getData();
+  render();
+});
+window.onload = render();
